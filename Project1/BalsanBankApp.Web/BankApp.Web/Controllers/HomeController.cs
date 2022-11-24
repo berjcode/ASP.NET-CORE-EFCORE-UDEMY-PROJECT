d0433c912d0f04a8a0e18db2edf8e1c55d9f1ebd
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-11-24 21:32:14 +0300

    Add TagHelpers

--- Project1/BalsanBankApp.Web/BankApp.Web/Controllers/HomeController.cs
+++ Project1/BalsanBankApp.Web/BankApp.Web/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BankApp.Web.Data.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BankApp.Web.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly BankContext _context;
+
+        public HomeController(BankContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Users.ToList());
         }
     }
 }
---
+++ Project1/BalsanBankApp.Web/BankApp.Web/Tag Helpers/GetBankAccount.cs
@@ -0,0 +1,24 @@
+﻿using BankApp.Web.Data.Context;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BankApp.Web.Tag_Helpers
+{
+    [HtmlTargetElement("getBankAccount")]
+    public class GetBankAccount : TagHelper
+    {
+        public int UserID { get; set; }
+        private readonly BankContext _context;
+
+        public GetBankAccount(BankContext context)
+        {
+            _context = context;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var accountCount = _context.Accounts.Count(x=> x.Id == UserID);
+            var html = $"<span class='badge bg-success'> {accountCount} </span>";
+            output.Content.SetHtmlContent(html);
+        }
+    }
+}
