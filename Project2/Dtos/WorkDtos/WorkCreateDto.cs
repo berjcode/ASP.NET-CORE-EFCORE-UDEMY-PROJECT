2b870019672545cdb43c270d54b4d190640aa2b6
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-12-03 12:46:53 +0300

    Created Refactor IDto

--- Project2/Dtos/WorkDtos/WorkCreateDto.cs
+++ Project2/Dtos/WorkDtos/WorkCreateDto.cs
@@ -1,15 +1,19 @@
-﻿using System;
+﻿using Dtos.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Dtos.WorkDtos
 {
-   public class WorkCreateDto
+   public class WorkCreateDto:IDto
     {
 
+        [Required(ErrorMessage ="Doldurmak zorunlu")] 
         public string Definition { get; set; }
+
         public bool IsCompleted { get; set; }
     }
 }
--- Project2/Dtos/WorkDtos/WorkUpdateDto.cs
+++ Project2/Dtos/WorkDtos/WorkUpdateDto.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Dtos.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Dtos.WorkDtos
 {
-    public class WorkUpdateDto
+    public class WorkUpdateDto: IDto
     {
+        [Range(1,int.MaxValue,ErrorMessage ="Id zorunlu")]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Zorunlu")]
         public string Definition { get; set; }
         public bool IsCompleted { get; set; }
     }
