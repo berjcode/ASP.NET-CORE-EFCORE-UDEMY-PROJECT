2ee67046050771a56f5df3319e61fbd0d640bfab
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-12-04 11:26:38 +0300

    Updated  Class Response Object

--- Project2/Business/Services/WorkService.cs
+++ Project2/Business/Services/WorkService.cs
@@ -1,7 +1,12 @@
-﻿using Business.Interfaces;
+﻿using AutoMapper;
+using Business.Extension;
+using Business.Interfaces;
+using Common.ResponseObject;
 using DataAcces.UnitOfWork;
+using Dtos.Interfaces;
 using Dtos.WorkDtos;
 using Entities.Concrete;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,28 +18,73 @@ namespace Business.Services
     public class WorkService : IWorkServices
     {
        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly IValidator<WorkUpdateDto> _UpdateDtoValidator;
+        private readonly IValidator<WorkCreateDto> _createDtoValidator;
 
-        public WorkService(IUnitOfWork unitOfWork)
+        public WorkService(IUnitOfWork unitOfWork, IMapper mapper, IValidator<WorkUpdateDto> updateDtoValidator, IValidator<WorkCreateDto> createDtoValidator)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _UpdateDtoValidator = updateDtoValidator;
+            _createDtoValidator = createDtoValidator;
         }
 
+        // AutoMapperÖncesi
+        /*
         public async Task Create(WorkCreateDto dto)
         {
-          await  _unitOfWork.GetRepository<Work>().Create(new()
+            await _unitOfWork.GetRepository<Work>().Create(new()
             {
-              IsCompleted = dto.IsCompleted,
-              Definition=dto.Definition,
+                IsCompleted = dto.IsCompleted,
+                Definition = dto.Definition,
             });
-          await  _unitOfWork.SaveChanges();
+            await _unitOfWork.SaveChanges();
+        }*/
+
+
+        //AutoMapperSonrası
+
+        public async Task<IResponse<WorkCreateDto>> Create(WorkCreateDto dto)
+        {
+            var validationResult = _createDtoValidator.Validate(dto);
+            if(validationResult.IsValid)
+            {
+                await _unitOfWork.GetRepository<Work>().Create(_mapper.Map<Work>(dto));
+                await _unitOfWork.SaveChanges();
+                return new Response<WorkCreateDto>(ResponseType.Succes, dto);
+
+            }
+            else
+            {
+
+
+                return new Response<WorkCreateDto>(ResponseType.ValidationError, dto, validationResult.ConvertToCustomValidationError());
+                  
+    //            Validasyon ayrı class tutmadan önce
+    //            List<CustomValidationError> errors = new();
+    //            foreach(var error in validationResult.Errors)
+    //            {
+    //                errors.Add(new ()
+    //                {
+    //                    ErrorMessage =error.ErrorMessage,
+    //                    PropertyName = error.PropertyName
+    //});
+    //            }
+                //return new Response<WorkCreateDto>(ResponseType.ValidationError,dto,errors);
+            }
+
         }
 
+
+        //AutoMapperÖncesi
+        /*
         public async Task<List<WorkListDto>> GetAll()
         {
-           var list = await  _unitOfWork.GetRepository<Work>().GetAll();
-            var workList = new List<WorkListDto>(); 
+            var list = await _unitOfWork.GetRepository<Work>().GetAll();
+            var workList = new List<WorkListDto>();
 
-            if(list != null  && list.Count > 0 )
+            if (list != null && list.Count > 0)
             {
                 foreach (var work in list)
                 {
@@ -47,28 +97,62 @@ namespace Business.Services
                 }
             }
             return workList;
+        }*/
+
+        //AutoMapper Sonrası
+        public async Task<IResponse<List<WorkListDto>>> GetAll()
+        {
+          var data =  _mapper.Map<List<WorkListDto>>(await _unitOfWork.GetRepository<Work>().GetAll());
+            return new Response<List<WorkListDto>>(ResponseType.Succes,data);
         }
 
-        public async Task<WorkListDto> GetById(object id)
-        {
-            var work = await _unitOfWork.GetRepository<Work>().GetById(id);
+        public async Task<IResponse<IDto>> GetById<IDto>(int id)
+        {//AutoMapper Öncesi
+            /*
+            var work = await _unitOfWork.GetRepository<Work>().GetByFilter(x=>x.Id==id); //frame work ramden dispose edilene kadar izler. buna bir çözüm. İzlemeden bize data dönecek.
             return new()
             {
                 Definition = work.Definition,
-
                 IsCompleted = work.IsCompleted,
-            };
+            };*/
+
+            //AutoMapper Sonrası 
+            //Response Object oncesi 
+           // return _mapper.Map<IDto>(await _unitOfWork.GetRepository<Work>().GetByFilter(x=> x.Id ==id));
+
+
+            var data = _mapper.Map<IDto>(await _unitOfWork.GetRepository<Work>().GetByFilter(x => x.Id == id));
+            if(data == null)
+            {
+                return new Response<IDto>(ResponseType.NotFound, $"{id} ye ait data bulunamadı.");
+            }
+            return new Response<IDto>(ResponseType.Succes,data);
         }
 
-        public async Task Remove(object id)
+        public async Task<IResponse> Remove(int id)
         {
-            var deletedWork = await _unitOfWork.GetRepository<Work>().GetById(id);
-            _unitOfWork.GetRepository<Work>().Remove(deletedWork);
-            await  _unitOfWork.SaveChanges();
+            //AutoMapper Öncesi
+
+            // var deletedWork = await _unitOfWork.GetRepository<Work>().GetById(id);
+
+           var removedEntity= await _unitOfWork.GetRepository<Work>().GetByFilter(x  => x.Id == id);
+          if(removedEntity !=null )
+            {
+                _unitOfWork.GetRepository<Work>().Remove(removedEntity);
+                 await _unitOfWork.SaveChanges();
+
+
+                return new Response(ResponseType.Succes);
+
+            }
+            return new Response(ResponseType.NotFound, $" {id}' ye ait değer Bulunamadı");
         }
 
-        public async Task Update(WorkUpdateDto dto)
+        public async Task<IResponse<WorkUpdateDto>> Update(WorkUpdateDto dto)
         {
+
+            //AutoMapper Öncesi
+            /*
             _unitOfWork.GetRepository<Work>().Update(new()
             {
                 Definition = dto.Definition,
@@ -77,7 +161,37 @@ namespace Business.Services
 
             });
 
-            await _unitOfWork.SaveChanges();
+            await _unitOfWork.SaveChanges();*/
+
+            //AutoMapper Sonrası
+
+            var results= _UpdateDtoValidator.Validate(dto);
+            if(results.IsValid)
+            {
+                var updatedEntity = await _unitOfWork.GetRepository<Work>().Find(dto.Id);
+                if(updatedEntity != null )
+                {
+                    _unitOfWork.GetRepository<Work>().Update(_mapper.Map<Work>(dto),updatedEntity);
+
+                    await _unitOfWork.SaveChanges();
+                    return new Response<WorkUpdateDto>(ResponseType.Succes,dto);
+                }
+                return new Response<WorkUpdateDto>(ResponseType.NotFound, $" {dto.Id}' ye ait değer Bulunamadı");
+
+            }else
+            {
+                //List<CustomValidationError> errors = new();
+                //foreach (var error in results.Errors)
+                //{
+                //    errors.Add(new()
+                //    {
+                //        ErrorMessage = error.ErrorMessage,
+                //        PropertyName = error.PropertyName
+                //    });
+                //}
+                return new Response<WorkUpdateDto>(ResponseType.ValidationError, dto, results.ConvertToCustomValidationError());
+            }
+
         }
     }
 }
