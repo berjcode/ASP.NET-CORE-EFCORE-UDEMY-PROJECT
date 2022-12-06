02170d917dc459a57c4c07b8007b8a04d99d0a05
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-12-06 21:31:50 +0300

    .

--- Project3/AdvertisementApp/AdvertisementApp.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
+++ Project3/AdvertisementApp/AdvertisementApp.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -1,6 +1,10 @@
-﻿using AdvertisementApp.DataAccess.Contexts;
+﻿using AdvertisementApp.Business.ValidationRules.ProvidedServiceRules;
+using AdvertisementApp.DataAccess.Contexts;
 using AdvertisementApp.DataAccess.Interfaces;
+using AdvertisementApp.DataAccess.UnitOfWork;
+using AdvertisementApp.Dtos.ProvidedServicesDtos;
 using AutoMapper;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -22,11 +26,13 @@ namespace AdvertisementApp.Business.DependencyResolvers.Microsoft
 
            var mapperConfigurations =  new MapperConfiguration(opt =>
            {
-
-           });
+             // opt.AddProfile();
+               ;           });
             var mapper= mapperConfigurations.CreateMapper();
             services.AddSingleton(mapper);
-            services.AddScoped<IUnitOfWork, UnitOfWork>
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<IValidator<ProvidedServiceCreateDto>, ProvidedServiceCreateDtoValidator>();
+            services.AddTransient<IValidator<ProvidedServiceUpdateDto>, ProvidedServiceUpdateValidator>();
         }
     }
 }
---
+++ Project3/AdvertisementApp/AdvertisementApp.Business/ValidationRules/ProvidedServiceRules/ProvidedServiceUpdateValidator.cs
@@ -0,0 +1,23 @@
+﻿using AdvertisementApp.Dtos.ProvidedServicesDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Business.ValidationRules.ProvidedServiceRules
+{
+   public class ProvidedServiceUpdateValidator : AbstractValidator<ProvidedServiceUpdateDto>
+    {
+
+        public ProvidedServiceUpdateValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x=> x.Description).NotEmpty();
+            RuleFor(x=> x.Title).NotEmpty();
+            RuleFor(x=> x.ImagePath).NotEmpty();
+        }
+    }
+
+}
