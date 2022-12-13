86b736167b5a85f6b96b93999484212bfc71e2d2
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-12-13 12:33:05 +0300

    CreatedCategoryTables

--- WebApiProject/WebApi/WebApi/Data/Product.cs
+++ WebApiProject/WebApi/WebApi/Data/Product.cs
@@ -9,5 +9,8 @@
         public DateTime CreatedDate { get; set; } = DateTime.Now;
 
         public string  ImagePath { get; set; }
+
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
