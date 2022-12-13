7cea26b3fb933b8dfaec6b025927035b28149071
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-12-13 12:32:48 +0300

    CQRS Configuration

--- WebApiProject/WebApi/WebApi/Program.cs
+++ WebApiProject/WebApi/WebApi/Program.cs
@@ -19,7 +19,11 @@ builder.Services.AddCors(cors =>
         opt.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
     });
 });
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(opt =>
+{
+    opt.SerializerSettings.ReferenceLoopHandling =Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
