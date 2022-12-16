89cd148955971519863b791e7911e272061f5b00
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-12-16 11:06:35 +0300

    Update

---
+++ ASP.NET Web Form-Ado.net/FirstApplication/FirstApplication/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FirstApplication
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+       
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Application["TotalApplications"]= 0;
+            Application["TotalUserSessions"] = 0;
+            Application["TotalApplications"] = (int)Application["TotalApplications"] +1;
+
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Application["TotalUserSessions"] = (int)Application["TotalUserSessions"] +1 ;
+        }
+
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+            Application["TotalUserSessions"] = (int)Application["TotalUserSessions"] - 1;
+        }
+
+    }
+}
---
+++ ASP.NET Web Form-Ado.net/FirstApplication/FirstApplication/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="FirstApplication.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"> Buraya Giriş yapın
+
+        </asp:TextBox>
+        <p>
+       <asp:Button runat="server" Text="Click Me" OnClick="Unnamed1_Click" Width="118px" />
+        </p>
+    </form>
+</body>
+
+</html>
